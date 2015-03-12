55dc3c3fad6a32166974073d06603a808843b57e
chubby1968 <chubbi1968@gmail.com>
2015-03-12 09:43:09 +0100

    Updated Fighter

--- game/scripts/vscripts/barebones.lua
+++ game/scripts/vscripts/barebones.lua
@@ -266,11 +266,7 @@ function GameMode:OnHeroInGame(hero)
 	table.insert(self.vPlayers, hero)
 
 	-- This line for example will set the starting gold of every hero to 500 unreliable gold
-	hero:SetGold(500, false)
-
-	-- These lines will create an item and add it to the player, effectively ensuring they start with the item
-	local item = CreateItem("item_example_item", hero, hero)
-	hero:AddItem(item)
+	hero:SetGold(500, false)	
 end
 
 --[[
--- game/scripts/vscripts/heroes.lua
+++ game/scripts/vscripts/heroes.lua
@@ -1,8 +1,16 @@
--- Grant Sven his first skill
+-- Grant the fighter his starting equipment
 function GameMode:OnHeroInGame(hero)
 
+	if hero:GetUnitName() == "npc_dota_hero_fighter" then
+		-- These lines will create an item and add it to the player, effectively ensuring they start with the item
+		local item = CreateItem("item_chainmail", hero, hero)
+		hero:AddItem(item)
+		item = CreateItem("item_broadsword", hero, hero)
+		hero:AddItem(item) 
+	return end
+
 	-- Level the first ability
-	hero:GetAbilityByIndex(0):SetLevel(1)
+	-- hero:GetAbilityByIndex(0):SetLevel(1)
 end
 
 function teleport( keys )
@@ -39,24 +47,4 @@ function bullrush( keys )
 
 	end)
 
-end
-
-ListenToGameEvent("dota_glyph_used", Dynamic_Wrap(HeroLineWarsMode, "OnGlyphUsed"), self)
-
-function OnGlyphUsed(keys)
-	print("OnGlyphUsed!")
-	local team = keys.teamnumber
-	for k, creep in pairs(FindUnitsInRadius(team, self.hlw_ancient_position[team], nil, 600, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_NOT_SUMMONED, 0, false)) do
-		if creep:HasModifier("dummy_modifier") == false then
-			creep:AddNewModifier(creep, nil, "modifier_knockback", {duration = 0.5, should_stun = 0, knockback_distance = 1200, knockback_height = 300, center_x = self.hlw_ancient_position[team].x, center_y = self.hlw_ancient_position[team].y, center_z = self.hlw_ancient_position[team].z, knockback_duration = 1})
-		end
-	end
-	ExecuteOrderFromTable(
-	{
-		UnitIndex = self.fortifyStunner[team]:entindex(),
-		OrderType = DOTA_UNIT_ORDER_CAST_NO_TARGET,
-		AbilityIndex = self.fortifyStunner[team]:FindAbilityByName("fortify_stun"):entindex(),
-		Queue = true
-	})
-	EmitGlobalSound("Hero_ElderTitan.EchoStomp")
 end
